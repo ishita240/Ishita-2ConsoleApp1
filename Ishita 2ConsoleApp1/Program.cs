325a296c945d09068cecbb3dbbc5c71cef764b81
A <A@ANUJ>
2023-09-30 21:13:38 -0400

    Add project files.

---
+++ Ishita 2ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishita_2ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumTriple(1, 2));
+            Console.WriteLine(SumTriple(3, 2));
+            Console.WriteLine(SumTriple(2, 2));
+            Console.ReadLine();
+        }
+        public static int SumTriple(int a, int b)
+        {
+            return a == b ? (a + b) * 3 : a + b;
+            
+            
+        }
+
+    }
+}
